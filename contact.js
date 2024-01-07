0c89c2d46e3705d533464e5cdb673832e3dff1a6
came989 <45994084+came989@users.noreply.github.com>
2024-01-07 15:44:07 +0200

    Update contact.js

--- contact.js
+++ contact.js
@@ -4,10 +4,8 @@ document.getElementById("submit").onclick = function(){
    var email = document.getElementById("email").value
    var subject = document.getElementById("subject").value
    var message = document.getElementById("message").value
-   alert(`${namee} ${email} ${subject} ${message}`); //testez sa vad ca se stocheaza string-ul
-    var service1 = 0;
-    var service2 = 0;
-    var service3 = 0;
+   //alert(`${namee} ${email} ${subject} ${message}`); //testez sa vad ca se stocheaza string-ul
+  
     var service = 0;
 
     if(document.getElementById("vehicle1").checked && document.getElementById("vehicle2").checked && document.getElementById("vehicle3").checked){
@@ -25,7 +23,7 @@ document.getElementById("submit").onclick = function(){
     } else if(document.getElementById("vehicle3").checked) {
         service = 100000
     }
-    //alert(`${service}`);
+    alert(`Your Total price for the selected services is ${service}`);
    /* var product = 0 
   var vehicle1 = document.getElementById("vehicle1").value
   var vehicle2 = document.getElementById("vehicle2").value
